b6f97efd32365fa4d370fe7300a63d5b0059ccde
jonatronblah <jonatron@gmail.com>
2020-10-25 10:19:52 -0400

    time series work

--- services/web/app/__init__.py
+++ services/web/app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
+from redistimeseries.client import Client
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
+
+client = Client(host='redistimeseries', port=6379, db=0, decode_responses=True)
+
+client.create('temperature')
+
 
 def create_app():
     app = Flask(__name__)
--- services/web/app/models.py
+++ services/web/app/models.py
@@ -1,9 +1,8 @@
-from .dao import RedisObject
-import json
-import redis
+from redistimeseries.client import Client
 
-class User(RedisObject):
-    def __init__(self, first, last, id = None):
+
+class User(object):
+    def __init__(self, time, last, id = None):
         RedisObject.__init__(self, id)
         self.first = first
         self.last = last
@@ -11,5 +10,18 @@ class User(RedisObject):
     def setitem(self):
         payload = {'first':self.first,'last':self.last}
         self.client.hmset(self.id, payload)
+        
+        
+        
+class Data(object):
+    def __init__(self, timestamp, temperature, humidity):
+        self.timestamp = timestamp
+        self.temperature = temperature
+        self.humidity = humidity
+        
+    def setitem(self):
+        client = Client(host='redistimeseries', port=6379, db=0, decode_responses=True)
+        client.add('temperature', timestamp, temperature)
+        client.add('humidity', timestamp, humidity)
         
     
